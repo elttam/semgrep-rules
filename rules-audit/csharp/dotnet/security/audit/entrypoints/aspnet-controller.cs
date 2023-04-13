94b17533dc3d6b60447b887126e71f6fca6f56ac
Ben Cambourne <ben@elttam.com>
2023-04-13 16:56:20 +1000

    [Rules] C# ASP.NET entrypoint rule
    Co-authored-by: joseph-elttam <112832107+joseph-elttam@users.noreply.github.com>

---
+++ rules-audit/csharp/dotnet/security/audit/entrypoints/aspnet-controller.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TestingMVC.Models;
+using TestingMVC.Models.AccountViewModels;
+using TestingMVC.Services;
+
+namespace TestingMVC.Controllers
+{
+    [Route("[controller]/[action]")]
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger _logger;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        // ruleid: aspnet-controller-entrypoint
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl = null)
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        // ruleid: aspnet-controller-entrypoint
+        [HttpPost("/test")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Test(string val = null)
+        {
+            return 1;
+        }
+    }
+}
